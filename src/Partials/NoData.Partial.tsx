b4ecdc903b4b7d34ba8c540b90f1d584127eac95
Pablo Alves <=>
2022-11-11 14:26:42 -0300

    finalizando NoData

--- src/Partials/NoData.Partial.tsx
+++ src/Partials/NoData.Partial.tsx
@@ -10,8 +10,8 @@ const NoDataPartial: React.FC = () => {
     const {theme} = useAppContext();
     const primaryColor = Themes[theme].primary.default;
     return(
-        <Section height={500} primaryColor={primaryColor} title="NoData">
-            <Label>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, necessitatibus laudantium aliquid sunt est quis dolorem deleniti. Voluptatibus cum praesentium dolores, cupiditate nemo ullam, fugiat, architecto ipsa eos laboriosam aliquam.</Label>
+        <Section primaryColor={primaryColor} title="NoData">
+            <Label>Um container em branco para tabelas que nao possuem dados.</Label>
             <NoData />
         </Section>
     )
